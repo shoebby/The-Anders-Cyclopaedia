c9f2894d127bc885e165740c276968f02aee501c
Alex <alexandria.vaneekelen@gmail.com>
2023-10-23 20:56:24 +0200

    dialogue additions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,21 +17,20 @@ public class GameManager : Singleton<GameManager>, IObserver
     public void SetGameState(GameState state)
     {
         currentGameState = state;
-
     }
+
     public void Pause() => Time.timeScale = 0f;
     public void Unpause() => Time.timeScale = 1f;
     public void RestartLevel() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     public void QuitGame() => Application.Quit();
 
-    #region Observer Pattern Stuff
+    #region Observer Functionality
 
     [SerializeField] Subject sampleSubject;
 
     public void OnNotify()
     {
         throw new System.NotImplementedException();
-        //do something idk how this design pattern works
     }
 
     private void OnEnable() => sampleSubject.AddObserver(this);
