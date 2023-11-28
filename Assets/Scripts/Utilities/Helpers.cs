db62999d5c123e1983370e62466c784440598680
Alex <alexandria.vaneekelen@gmail.com>
2023-11-28 16:18:33 +0100

    forgot to remove these

--- Assets/Scripts/Utilities/Helpers.cs
+++ Assets/Scripts/Utilities/Helpers.cs
@@ -6,9 +6,6 @@ public static class Helpers
 {
     public static string previousScene;
 
-    public static float ambienceVolume;
-    public static float clipVolume;
-
     public static void DestroyChildren(this Transform t)
     {
         foreach (Transform child in t) Object.Destroy(child.gameObject);
